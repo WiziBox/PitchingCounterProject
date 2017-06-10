1a038c664ce74230c2062a1ac5b8ed40ea526d88
Lee Sanghyun <thslr2020@gmail.com>
2017-06-10 15:42:37 +0900

    기능적인 부분은 거의 완성 (UI, 리스트 구현?)

--- app/src/main/java/kr/misty/pitchingcounterapp/MainActivity.java
+++ app/src/main/java/kr/misty/pitchingcounterapp/MainActivity.java
@@ -20,9 +20,14 @@ public class MainActivity extends AppCompatActivity {
     Button button_strike;
     Button button_ball;
     Button button_cancel;
+    TextView textView1;
+    TextView textView2;
     TextView textView3;
     TextView textView4;
-    long elapsed_time;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    static long elapsed_time;
     long current_time;
 
     @Override
@@ -33,25 +38,62 @@ public class MainActivity extends AppCompatActivity {
         button_strike = (Button)findViewById(R.id.button_strike);
         button_ball = (Button)findViewById(R.id.button_ball);
         button_cancel = (Button)findViewById(R.id.button_cancel);
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
         textView3 = (TextView)findViewById(R.id.textView3);
         textView4 = (TextView)findViewById(R.id.textView4);
+        textView5 = (TextView)findViewById(R.id.textView5);
+        textView6 = (TextView)findViewById(R.id.textView6);
+        textView7 = (TextView)findViewById(R.id.textView7);
+
+        textView3.setText("던진 공\n"+ String.valueOf(Velocity.count)+ "구");
+        textView4.setText(String.format("평균 구속 \n %.0f km/h", Velocity.average));
+        textView5.setText("스트라이크\n" + String.valueOf(StrikeOrBall.strike));
+        textView6.setText("볼\n" + String.valueOf(StrikeOrBall.ball));
+
 
         button_startstop.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 if (button_startstop.isChecked()) {
                     current_time = System.currentTimeMillis();
+                    textView2.setText("측정중...");
                 } else {
                     elapsed_time = System.currentTimeMillis() - current_time;
                     float velocity = (float)elapsed_time;
-                    textView3.setText(String.format("측정된 구속 : %.2f km/h", 3600 * (18.44 / 1000 / (velocity / 1000))));
-                    textView4.setText(String.format(String.valueOf(velocity)));
+                    textView1.setText(String.valueOf(" "+Velocity.getInfo(elapsed_time)));
+                    textView2.setText(String.format("측정된 구속 : %.2f km/h", Velocity.getVelocity(elapsed_time)));
+                    textView3.setText("던진 공\n"+ String.valueOf(Velocity.count)+ "구");
+                    textView4.setText(String.format("평균 구속 \n %.0f km/h", Velocity.average));
+                    textView7.setText(String.valueOf(String.valueOf(elapsed_time)));
                 }
+            }
+        });
+
+        button_strike.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                StrikeOrBall.getResult(1);
+                textView5.setText("스트라이크\n" + String.valueOf(StrikeOrBall.strike));
+            }
+        });
+
+        button_ball.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                StrikeOrBall.getResult(2);
+                textView6.setText("볼\n" + String.valueOf(StrikeOrBall.ball));
             }
         });
 
         button_cancel.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                chrono_chronometer.setBase(SystemClock.elapsedRealtime());
+               StrikeOrBall.ball = 0;
+               StrikeOrBall.strike = 0;
+               Velocity.average = 0;
+               Velocity.count = 0;
+               Velocity.sum_velocity = 0;
+                textView3.setText("던진 공\n"+ String.valueOf(Velocity.count)+ "구");
+                textView4.setText(String.format("평균 구속 \n %.0f km/h", Velocity.average));
+                textView5.setText("스트라이크\n" + String.valueOf(StrikeOrBall.strike));
+                textView6.setText("볼\n" + String.valueOf(StrikeOrBall.ball));
             }
         });
 
--- app/src/main/java/kr/misty/pitchingcounterapp/Velocity.java
+++ app/src/main/java/kr/misty/pitchingcounterapp/Velocity.java
@@ -11,6 +11,8 @@ public class Velocity {
     public static double distance = 18.44;
     public static double average;
     public static double seconds;
+    public static int fastball = 0;
+    public static int breaking = 0;
 
     Velocity(double seconds) { ;
         Velocity.seconds = seconds;
@@ -18,18 +20,22 @@ public class Velocity {
 
     public static double getVelocity (double seconds){
         //distance = 18.44;
-        velocity = 3600 * (distance / 1000 / seconds);
+        seconds = MainActivity.elapsed_time;
+        velocity = 3600 * (distance  / 1000 / (seconds / 1000));
         return velocity;
     }
 
     public static String getInfo (double seconds){
         count++;
+        seconds = MainActivity.elapsed_time;
         sum_velocity += getVelocity(seconds);
         average = sum_velocity / count;
         if (getVelocity(seconds) + 5 >= average) {
-            return "직구입니다!";
+            fastball++;
+            return "직구";
         } else {
-            return "변화구입니다!";
+            breaking++;
+            return "변화";
         }
     }
 }
